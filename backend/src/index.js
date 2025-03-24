5658fe44f2521b1b2d7d8d8ce70ad7b4ba812020
Vishnu Vardhan Kolli <kollivishnu@Vishnus-MacBook-Pro.local>
2025-03-24 12:12:56 +0530

    tryyy

--- backend/src/index.js
+++ backend/src/index.js
@@ -43,13 +43,6 @@ app.use(
 app.use("/", authRoutes);
 
 // ✅ Serve frontend if in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist"))); // ✅ Vite builds to /dist
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
-  });
-}
 
 app.listen(PORT, () => {
   console.log("server is running on port:", PORT);
